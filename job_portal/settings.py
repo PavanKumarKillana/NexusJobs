cc8b999a9dcaffbbb70fe446461e1fb4bbb0e88b
Killana PavanKumar <pa1officialqueries@gmail.com>
2026-07-14 23:34:20 +0530

    Switch database backend to MySQL using PyMySQL

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'job_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nexusjobs_db',
+        'USER': 'root',
+        'PASSWORD': 'Pavan@5391',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
